42b91bb3edfb6dd0c1b0e1f23b62ff79cfc6ec15
Oliver Crawford <o0561319@amarillocollege.com>
2023-05-03 11:13:51 -0500

    Initial Commit

---
+++ test project/test_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_projectApp.swift
+//  test project
+//
+//  Created by AC Student on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct test_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
